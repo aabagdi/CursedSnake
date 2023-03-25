b0ec1a51ee1dfd23442b2993ecfee565d6bc7ba8
aabagdi <70984836+aabagdi@users.noreply.github.com>
2023-03-24 22:25:18 -0700

    Added color change option thanks to Simacd cursedsnake

---
+++ CursedSnake/ViewsAndScenes/Settings/SnakeColorView.swift
@@ -0,0 +1,20 @@
+//
+//  SnakeColorView.swift
+//  CursedSnake
+//
+//  Created by Aadit Bagdi on 3/24/23.
+//
+
+import SwiftUI
+
+struct SnakeColorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SnakeColorView_Previews: PreviewProvider {
+    static var previews: some View {
+        SnakeColorView()
+    }
+}
